b13deaf05a1d73575335af4c581aab2065bc822a
homhom2012z <homhom2012z@gmail.com>
2022-04-11 07:20:08 +0700

    change(projects-component): use react-json-view instead of a custom pre tag

--- components/Projects/ProjectCardJSON.js
+++ components/Projects/ProjectCardJSON.js
@@ -1,12 +1,22 @@
 import { Box, Stack, Code } from "@chakra-ui/react";
+import loadable from "@loadable/component";
+const ReactJson = loadable(() => import("react-json-view"));
 
 const ProjectCardJSON = ({ data, id }) => {
   return (
     <>
       <Box maxWidth={"container.md"}>
-        <pre style={{ whiteSpace: "pre-wrap" }}>
+        {/* <pre style={{ whiteSpace: "pre-wrap" }}>
           {JSON.stringify(data, null, 2)}
-        </pre>
+        </pre> */}
+        <ReactJson
+          name={false}
+          displayObjectSize={false}
+          displayDataTypes={false}
+          enableClipboard={false}
+          src={data}
+          theme="rjv-default"
+        />
       </Box>
     </>
   );
